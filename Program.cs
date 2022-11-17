469c42f4dad8e49b336b2c0a5927a36aad1e04fd
kamilkostrzewski <kamilkostrzewski@itsharkz.com>
2022-11-18 00:40:53 +0100

    Compatibility fixed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("");
+if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    throw new InvalidOperationException("It works only on Linux.");
+}
